b1ee54b01882478e0561fae1b7a4bd1f20735694
Tejas Nasa <tejasnasa2005@gmail.com>
2026-07-13 16:49:27 +0530

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for (int l = 0, r = 0; r < nums.size(); r++) {
+            if (nums[r]) {
+                swap(nums[l++], nums[r]);
+            }
+        }
+    }
+};
