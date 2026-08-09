8e1f9ca19775ccc97aee24e8f45909e90cefd2d6
Tejas Nasa <tejasnasa2005@gmail.com>
2026-08-09 19:13:35 +0530

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.cpp
@@ -0,0 +1,37 @@
+#include <cstring>
+
+class Solution {
+public:
+    int dp[1005][1005];
+
+    bool pal(string &s, int i, int j) {
+        if (i>=j) return true;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        return dp[i][j] = (s[i] == s[j] && pal(s, i+1, j-1));
+    }
+
+    string longestPalindrome(string s) {
+        memset(dp, -1, sizeof(dp));
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                pal(s,i,j);
+            }
+        }
+
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (dp[i][j] && (j - i + 1) > maxLen) {
+                    start = i;
+                    maxLen = j - i + 1;
+                }
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
