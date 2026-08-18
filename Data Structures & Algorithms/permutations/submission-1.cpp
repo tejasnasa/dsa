c98e9f46a258e3b3391a4dd5eb4f251fb6df8404
Tejas Nasa <tejasnasa2005@gmail.com>
2026-08-19 01:22:11 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+
+    void perm(vector<int> &nums, vector<int> &subset, vector<bool> &used) {
+        if (subset.size() == nums.size()) {
+            res.push_back(subset);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            subset.push_back(nums[i]);
+            
+            perm(nums, subset, used);
+
+            subset.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> subset;
+        vector<bool> used(nums.size(), false);
+
+        perm(nums, subset, used);
+
+        return res;
+    }
+};
