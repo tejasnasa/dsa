3f06c368993a7aa6668b63106ee83146658b9f4e
Tejas Nasa <tejasnasa2005@gmail.com>
2026-08-26 01:49:41 +0530

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int reverse(int X) {
+        vector<int> digits;
+        long long x = X;
+        bool neg = false;
+
+        if (x < 0) {
+            neg = true;
+            x = abs(x);
+        }
+
+        while (x != 0) {
+            digits.push_back(x % 10);
+            cout << x%10;
+            x /= 10;
+        }
+
+        std::reverse(digits.begin(), digits.end());
+
+        long long lo = INT_MIN, hi = INT_MAX, res = 0;
+
+        for (int i = 0; i < digits.size(); i++) {
+            res += digits[i]*pow(10, i);
+        }
+
+        if (neg) res = -res;
+
+        return (lo <= res && res <= hi) ? (int)res : 0;
+    }
+};
