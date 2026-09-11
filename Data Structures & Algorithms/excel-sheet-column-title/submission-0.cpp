90f6ac8f64dd5be95d3e3f363c604dd2d361be68
Tejas Nasa <tejasnasa2005@gmail.com>
2026-09-12 04:44:05 +0530

    Add: excel-sheet-column-title - submission-0

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        
+        if (columnNumber == 0) return "";
+
+        int n = columnNumber-1;
+        return convertToTitle(n / 26) + char('A' + n%26);
+    }
+};
